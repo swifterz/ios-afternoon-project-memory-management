527198fa0543aa711ec6bce3bf12d76ccb532c95
Enayatullah Naseri <enayatullahna@gmail.com>
2020-06-11 20:34:19 -0700

    Created Model

---
+++ ContactsMRC/ContactsMRC/LSIContacts.m
@@ -0,0 +1,16 @@
+//
+//  LSIContacts.m
+//  ContactsMRC
+//
+//  Created by Enayatullah Naseri on 6/11/20.
+//  Copyright © 2020 Enayatullah Naseri. All rights reserved.
+//
+
+#import "LSIContacts.h"
+
+@implementation LSIContacts
+
+
+
+
+@end
